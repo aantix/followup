eb56ba37c94140ac9b119e4b0628c35ccd143177
Jim Jones <jjones@aantix.com>
2014-09-12 01:09:21 -0700

    Changed to ActiveJobs wait syntax

--- scripts/schedule_reminders.rb
+++ scripts/schedule_reminders.rb
@@ -16,5 +16,5 @@ users.each do |user|
   time_at = Time.parse("#{Date.today} #{user.email_send.strftime("%I:%M %P")}").in_time_zone(zone)
 
   logger.info "Scheduling #{user.id} to update at #{time_at}"
-  FollowupJob.perform_later(time_at, user.id, true)
+  FollowupJob.(wait_until: time_at).perform_later(user.id, true)
 end
