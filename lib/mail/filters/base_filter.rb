78b928476bcabd03429c4aec8f5f03420060f5d1
Jim Jones <jim.jones1@gmail.com>
2015-08-25 22:27:36 -0500

    Changed base filter detection to return filtered phrase/k,v that is detected instead of a boolean.

--- lib/mail/filters/base_filter.rb
+++ lib/mail/filters/base_filter.rb
@@ -2,17 +2,17 @@ module Mail
   module Filters
     class BaseFilter
       def self.blacklisted_words?(blacklisted_phrases, text)
-        blacklisted_phrases.any?{|phrase| text =~ /#{phrase}/i}
+        blacklisted_phrases.detect{|phrase| text =~ /#{phrase}/i}
       end
 
       def self.blacklisted_key_value?(blacklisted_keys, headers)
-        blacklisted_keys.any? do |(k, v)|
+        blacklisted_keys.detect do |(k, v)|
           headers.keys.include?(k) && (header_hash[k] == v || v == :any)
         end
       end
 
       def self.blacklisted_name_value?(blacklisted_keys, headers)
-        blacklisted_keys.any? do |(k, v)|
+        blacklisted_keys.detect do |(k, v)|
           headers.any?{|h| h.name == k && (v == :any || h.value == v)}
         end
       end
